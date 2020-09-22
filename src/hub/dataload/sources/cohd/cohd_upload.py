46425a10300986d997ee7f3368120c01c4ea58ba
jiwen xin <kevinxin@scripps.edu>
2020-09-22 07:14:21 -0700

    udpate cohd

--- src/hub/dataload/sources/cohd/cohd_upload.py
+++ src/hub/dataload/sources/cohd/cohd_upload.py
@@ -2,10 +2,18 @@ import os.path
 from .cohd_parser import load_data
 import biothings.hub.dataload.uploader as uploader
 
+SRC_META = {
+    "url": "http://cohd.smart-api.info/",
+    "license_url": "",
+    "license_url_short": "",
+    "license": "CC BY-NC 4.0",
+}
+
 
 class COHDUploader(uploader.BaseSourceUploader):
 
     name = "cohd"
+    __metadata__ = {"src_meta": SRC_META}
 
     def load_data(self, data_folder):
         cohd_docs = load_data()
