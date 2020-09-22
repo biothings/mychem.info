22f48cc8626c4e69a8498be757153b41e7fb0208
jiwen xin <kevinxin@scripps.edu>
2020-09-22 06:47:15 -0700

    add cohd uploader

---
+++ src/hub/dataload/sources/cohd/cohd_upload.py
@@ -0,0 +1,31 @@
+import os.path
+from .cohd_parser import load_data
+import biothings.hub.dataload.uploader as uploader
+
+
+class COHDUploader(uploader.BaseSourceUploader):
+
+    name = "cohd"
+
+    def load_data(self, data_folder):
+        cohd_docs = load_data(data_folder)
+        return cohd_docs
+
+    @classmethod
+    def get_mapping(klass):
+        mapping = {
+            "cohd": {
+                "properties": {
+                    "cohd": {
+                        "type": "keyword",
+                        "normalizer": "keyword_lowercase_normalizer",
+                        'copy_to': ['all'],
+                    },
+                    "name": {
+                        "type": "keyword",
+                        "normalizer": "keyword_lowercase_normalizer"
+                    }
+                }
+            }
+        }
+        return mapping
