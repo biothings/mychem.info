6038c05c0a319d1aa1d4a525705417c717a4e056
jiwen xin <kevinxin@scripps.edu>
2020-09-22 06:58:13 -0700

    update cohd

--- src/hub/dataload/sources/cohd/cohd_upload.py
+++ src/hub/dataload/sources/cohd/cohd_upload.py
@@ -8,7 +8,7 @@ class COHDUploader(uploader.BaseSourceUploader):
     name = "cohd"
 
     def load_data(self, data_folder):
-        cohd_docs = load_data(data_folder)
+        cohd_docs = load_data()
         return cohd_docs
 
     @classmethod
