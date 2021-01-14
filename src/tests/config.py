2441bfbbbb0d87f23463014a8d44174f4abbc762
Zhongchao Qian <zqian@scripps.edu>
2021-01-13 22:27:18 -0800

    initial AppTests

---
+++ src/tests/config.py
@@ -0,0 +1,50 @@
+
+"""
+    Mychem.info
+    https://mychem.info/
+    Chemical and Drug Annotation as a Service.
+"""
+
+import re
+
+# *****************************************************************************
+# Elasticsearch variables
+# *****************************************************************************
+ES_HOST = 'localhost:9200'
+ES_INDEX = 'mychem_test'
+ES_DOC_TYPE = 'chem'
+ES_INDICES = {
+    "drug": ES_INDEX,
+    "compound": ES_INDEX,
+    "chem": ES_INDEX,
+}
+ES_SCROLL_TIME = '10m'
+
+# *****************************************************************************
+# App URL Patterns
+# *****************************************************************************
+
+GA_ACTION_QUERY_GET = 'query_get'
+GA_ACTION_QUERY_POST = 'query_post'
+GA_ACTION_ANNOTATION_GET = 'drug_get'
+GA_ACTION_ANNOTATION_POST = 'drug_post'
+GA_TRACKER_URL = 'c.biothings.io'
+
+# *****************************************************************************
+# Endpoint Specifics
+# *****************************************************************************
+
+ANNOTATION_ID_REGEX_LIST = [
+    (re.compile(r'db[0-9]+', re.I), 'drugbank.id'),
+    (re.compile(r'chembl[0-9]+', re.I), 'chembl.molecule_chembl_id'),
+    (re.compile(r'chebi\:[0-9]+', re.I), ['chebi.id', 'chebi.secondary_chebi_id']),
+    (re.compile(r'[A-Z0-9]{10}'), 'unii.unii'),
+    (re.compile(r'((cid\:(?P<search_term>[0-9]+))|([0-9]+))', re.I), 'pubchem.cid')
+]
+
+STATUS_CHECK = {
+    'id': 'USNINKBPBVKHHZ-CYUUQNCZSA-L',  # penicillin
+    'index': 'mydrugs_current',
+    'doc_type': 'drug'
+}
+
---
+++ src/tests/test_apptests.py
@@ -0,0 +1,158 @@
+import glob
+import json
+import os
+
+import pytest
+import elasticsearch
+from biothings.tests.web import BiothingsWebAppTest
+
+
+class TestMultiIndices(BiothingsWebAppTest):
+    def test_000_null(self):
+        pass
+
+
+class TestMyChemWebAppConfigAnnotationIdRegex(BiothingsWebAppTest):
+    def _process_es_data_dir(self, data_dir_path):
+        if not os.path.exists(data_dir_path):
+            yield
+            return
+
+        client = elasticsearch.Elasticsearch(self.settings.ES_HOST)
+        server_major_version = client.info()['version']['number'].split('.')[0]
+        client_major_version = str(elasticsearch.__version__[0])
+        if server_major_version != client_major_version:
+            pytest.exit('ES version does not match its python library.')
+
+        # FIXME: this is broken
+        default_doc_type = self.settings.ES_DOC_TYPE
+
+        indices = []
+        glob_json_pattern = os.path.join(data_dir_path, '*.json')
+        # FIXME: wrap around in try-finally so the index is guaranteed to be
+        #  cleaned up
+        for index_mapping_path in glob.glob(glob_json_pattern):
+            index_name = os.path.basename(index_mapping_path)
+            index_name = os.path.splitext(index_name)[0]
+            indices.append(index_name)
+            if client.indices.exists(index_name):
+                raise Exception(f"{index_name} already exists!")
+            with open(index_mapping_path, 'r') as f:
+                mapping = json.load(f)
+            data_path = os.path.join(data_dir_path, index_name + '.ndjson')
+            with open(data_path, 'r') as f:
+                bulk_data = f.read()
+            if elasticsearch.__version__[0] > 6:
+                client.indices.create(index_name, mapping, include_type_name=True)
+                client.bulk(bulk_data, index_name)
+            else:
+                client.indices.create(index_name, mapping)
+                # FIXME: doc_type problem in ES6
+                # client.bulk(bulk_data, index_name, default_doc_type)
+                client.bulk(bulk_data, index_name, "drug")
+
+            client.indices.refresh()
+        yield
+        for index_name in indices:
+            client.indices.delete(index_name)
+
+    @pytest.fixture(scope="class", autouse=True)
+    def load_es_data_cls(self, request):
+        data_dir = './test_data/' + request.cls.__name__
+
+        yield from self._process_es_data_dir(data_dir)
+
+    # reason why we do not want this is because this would screw up
+    # the indices loaded for the class/session/module
+    # @pytest.fixture(scope="function", autouse=True)
+    # def load_es_data_fn(self, request):
+    #     data_dir = './test_data/' + request.cls.__name__ + '/' + \
+    #         request.function.__name__
+    #     yield from self._process_es_data_dir(data_dir)
+
+    def test_010_drugbank(self):
+        q = 'db0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['drugbank']['id'].lower() == q
+
+    def test_011_chembl(self):
+        q = 'chembl0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['chembl']['molecule_chembl_id'].lower() == q
+
+    def test_012_chebi(self):
+        q = 'chebi:0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['chebi']['id'].lower() == q
+
+    def test_013_chebi_secondary(self):
+        q = 'chebi:2'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['chebi']['secondary_chebi_id'].lower() == q
+
+    def test_014_unii(self):
+        q = 'MOCKUNII00'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['unii']['unii'] == q
+
+    def test_015_pubchem(self):
+        q = 'cid:0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['pubchem']['cid'] == "0"
+
+    def test_016_pubchem_noprefix(self):
+        q = '0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['pubchem']['cid'] == "0"
+
+    def test_020_drugbank_ci(self):
+        q = 'dB0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['drugbank']['id'].lower() == q.lower()
+
+    def test_021_chembl_ci(self):
+        q = 'cHeMbL0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['chembl']['molecule_chembl_id'].lower() == q.lower()
+
+    def test_022_chebi_ci(self):
+        q = 'ChEBI:0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['chebi']['id'].lower() == q.lower()
+
+    def test_023_cid_ci(self):
+        q = 'Cid:0'
+        res = self.request("chem", method="POST", data={"ids": q})
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]['pubchem']['cid'] == "0"
+
+    def test_030_drugbank_db_in_default_scope(self):
+        # by default it looks at _id, but the query below
+        # matches the UNII regex, so nothing should be returned
+        q = 'DEFAULTSCO'
+        # FIXME: check response status code
+        res = self.request("chem", method="POST", data={"ids": q}, expect=200)
+        res = res.json()
+        assert len(res) == 1
+        assert res[0]["notfound"]
