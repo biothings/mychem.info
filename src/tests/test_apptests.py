a2919f59d91a05844763e677722020b548c5ef4d
Zhongchao Qian <zqian@scripps.edu>
2021-04-30 15:53:07 -0700

    use new value_in_result method

--- src/tests/test_apptests.py
+++ src/tests/test_apptests.py
@@ -9,85 +9,81 @@ class TestMyChemWebAppConfigAnnotationIdRegex(BiothingsWebAppTest):
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q in [x.lower() for x in
-                     self.get_all_nested(res[0], 'drugbank.id')]
+        assert self.value_in_result(q, res, 'drugbank.id', True)
 
     def test_011_chembl(self):
         q = 'chembl297569'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q in [x.lower() for x in
-                     self.get_all_nested(res[0], 'chembl.molecule_chembl_id')]
+        assert self.value_in_result(q, res, 'chembl.molecule_chembl_id', True)
 
     def test_012_chebi(self):
         q = 'chebi:57966'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q in [x.lower() for x in
-                     self.get_all_nested(res[0], 'chebi.id')]
+        assert self.value_in_result(q, res, 'chebi.id', True)
+
 
     def test_013_chebi_secondary(self):
         q = 'chebi:22821'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q in [x.lower() for x in
-                     self.get_all_nested(res[0], 'chebi.secondary_chebi_id')]
+        assert self.value_in_result(q, res, 'chebi.secondary_chebi_id', True)
 
     def test_014_unii(self):
         q = '11P2JDE17B'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q in self.get_all_nested(res[0], 'unii.unii')
+        assert self.value_in_result(q, res, 'unii.unii', False)
 
     def test_015_pubchem(self):
         q = 'cid:120933777'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert "120933777" in self.get_all_nested(res[0], 'pubchem.cid')
+        assert self.value_in_result("120933777", res, 'pubchem.cid')
 
     def test_016_pubchem_noprefix(self):
         q = '120933777'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert "120933777" in self.get_all_nested(res[0], 'pubchem.cid')
+        assert self.value_in_result(q, res, 'pubchem.cid')
 
     def test_020_drugbank_ci(self):
         q = 'dB03107'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q.lower() in [x.lower() for x in
-                             self.get_all_nested(res[0], 'drugbank.id')]
+        assert self.value_in_result(q, res, 'drugbank.id', True)
+
 
     def test_021_chembl_ci(self):
         q = 'CHEMBL297569'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q.lower() in [x.lower() for x in self.get_all_nested(
-            res[0], 'chembl.molecule_chembl_id'
-        )]
+        assert self.value_in_result(q, res, 'chembl.molecule_chembl_id', True)
+
 
     def test_022_chebi_ci(self):
         q = 'ChEBI:57966'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert q.lower() in [x.lower() for x in
-                             self.get_all_nested(res[0], 'chebi.id')]
+        assert self.value_in_result(q, res, 'chebi.id', True)
+
 
     def test_023_cid_ci(self):
         q = 'Cid:120933777'
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert "120933777" in self.get_all_nested(res[0], 'pubchem.cid')
+        assert self.value_in_result("120933777", res, 'pubchem.cid')
 
 
 class TestMyChemWebAppConfigAnnotationRegexMockData(BiothingsWebAppTest):
