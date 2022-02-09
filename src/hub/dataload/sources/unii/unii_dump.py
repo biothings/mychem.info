4f7944afd58adb036ab2f8c452c4dcf8760fb149
Ricardo Avila <ravila@protonmail.com>
2022-02-09 08:40:41 -0800

    Update unii plugin (#129)
    * Update UNII dumper
    
    * Update homepage url
    
    * Allow executing parser as a standalone module
    
    * Update mapping

--- src/hub/dataload/sources/unii/unii_dump.py
+++ src/hub/dataload/sources/unii/unii_dump.py
@@ -1,11 +1,11 @@
-import os, sys, re, time
+import datetime
+import os
+
 import bs4
 import dateutil.parser as dtparser
-import datetime
-
-from config import DATA_ARCHIVE_ROOT
-from biothings.hub.dataload.dumper import HTTPDumper, DumperException
+from biothings.hub.dataload.dumper import DumperException, HTTPDumper
 from biothings.utils.common import unzipall
+from config import DATA_ARCHIVE_ROOT
 
 
 class UniiDumper(HTTPDumper):
@@ -14,28 +14,34 @@ class UniiDumper(HTTPDumper):
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
 
     SCHEDULE = "0 12 * * *"
-    HOMEPAGE_URL = "https://fdasis.nlm.nih.gov/srs"
-    DATA_URL = "https://fdasis.nlm.nih.gov/srs/download/srs/UNII_Data.zip"
+    HOMEPAGE_URL = "https://precision.fda.gov/uniisearch/archive"
+    DATA_BASE_URL = "https://qnyqxh695c.execute-api.us-east-1.amazonaws.com/production/v1/get-file/"
 
     def get_latest_release(self):
         res = self.client.get(self.__class__.HOMEPAGE_URL)
-        html = bs4.BeautifulSoup(res.text,"lxml")
-        # link containing the latest date version
-        version = html.find(attrs={"href":"/srs/jsp/srs/uniiListDownload.jsp"}).text
-        m = re.match("UNII List download \(updated (.*)\)",version)
+        # Raise error if status is not 200
+        res.raise_for_status()
+        # Parse the html for the text in the first 'div' element under the archive accordion
+        html = bs4.BeautifulSoup(res.text, 'lxml')
+        archive = html.find("div", attrs={"class": "usa-accordion__content usa-prose"})
+        version = archive.find("div", attrs={"class": "styles__StyledDownloadLink-sc-pyispm-6 cdoIOb"}).text
         try:
-            latest = datetime.date.strftime(dtparser.parse(m.groups()[0]),"%Y-%m-%d")
+            latest = datetime.date.strftime(dtparser.parse(version), "%Y-%m-%d")
             return latest
         except Exception as e:
-            raise DumperException("Can't find or parse date from URL '%s': %s" % (self.__class__.HOMEPAGE_URL,e))
+            raise DumperException("Can't find or parse date from URL '%s': %s" % (self.__class__.HOMEPAGE_URL, e))
 
-    def create_todump_list(self,force=False,**kwargs):
+    def create_todump_list(self, force=False, **kwargs):
         self.release = self.get_latest_release()
-        if force or not self.src_doc or (self.src_doc and self.src_doc.get("download",{}).get("release") < self.release):
-            # if new release, that link points to that latest release
-            local = os.path.join(self.new_data_folder,os.path.basename(self.DATA_URL))
-            self.to_dump.append({"remote":self.DATA_URL, "local":local})
+        if force or not self.src_doc or (self.src_doc and self.src_doc.get("download", {}).get("release") < self.release):
+            # if new release, generate link that points to that latest release
+            res = self.client.get(
+                self.__class__.DATA_BASE_URL + "{}/UNII_Data_{}.zip".format(self.release, self.release.replace("-", ""))
+                )
+            res.raise_for_status()
+            data_url = res.json()['url']
+            local = os.path.join(self.new_data_folder, self.release)
+            self.to_dump.append({"remote": data_url, "local": local})
 
     def post_dump(self, *args, **kwargs):
         unzipall(self.new_data_folder)
-
