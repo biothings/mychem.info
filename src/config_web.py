0290a892095543e2e30ef4b8af231fdb27af3164
Ricardo Avila <ravila@protonmail.com>
2022-03-21 17:12:39 -0700

    Add drugbank and pharmgkb to list of annotation regexesw

--- src/config_web.py
+++ src/config_web.py
@@ -27,6 +27,8 @@ ANNOTATION_ID_REGEX_LIST = [
     (re.compile(r'chembl[0-9]+', re.I), 'chembl.molecule_chembl_id'),
     (re.compile(r'chebi\:[0-9]+', re.I), ['chebi.id', 'chebi.secondary_chebi_id']),
     (re.compile(r'[A-Z0-9]{10}'), 'unii.unii'),
+    (re.compile(r'db[0-9]+', re.I), ['unichem.drugbank', 'chebi.xrefs.drugbank', 'drugcentral.xrefs.drugbank_id', 'pharmgkb.xrefs.drugbank']),
+    (re.compile(r'pa[0-9]+', re.I), 'pharmgkb.id'),
     (re.compile(r'((cid\:(?P<term>[0-9]+))|([0-9]+))', re.I), ['pubchem.cid', 'fda_orphan_drug.pubchem_sid'])
 ]
 
