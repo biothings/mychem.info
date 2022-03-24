df4268f89efac986ae4f6ec68a45488c145c6516
Ricardo Avila <ravila@protonmail.com>
2022-03-24 08:56:42 -0700

    Add license and website link for UMLS plugin (#136)
    * Add drugbank and pharmgkb to list of annotation regexesw
    
    * Add drugbank and pharmgkb to annotations
    
    * Allow umls plugin to fetch latest release version
    
    * Update license & source metadata

--- src/hub/dataload/sources/umls/umls_upload.py
+++ src/hub/dataload/sources/umls/umls_upload.py
@@ -3,6 +3,11 @@ from .umls_parser import load_data
 import biothings.hub.dataload.uploader as uploader
 
 
+SRC_META = {
+        "url": 'https://www.nlm.nih.gov/research/umls/index.html',
+        "license_url" : "https://www.nlm.nih.gov/research/umls/knowledge_sources/metathesaurus/release/license_agreement.html"
+        }
+
 class UMLSUploader(uploader.BaseSourceUploader):
 
     name = "umls"
