1ae6ada31fd29937ee15ee4b96d9f2cb73a76d6d
Ricardo Avila <ravila@protonmail.com>
2022-04-20 00:05:46 -0700

    Optional CURIE prefixes in annotation regex

--- src/config_web.py
+++ src/config_web.py
@@ -22,12 +22,13 @@ ES_SCROLL_TIME = '10m'
 # *****************************************************************************
 
 ANNOTATION_ID_REGEX_LIST = [
-    (re.compile(r'chembl[0-9]+', re.I), 'chembl.molecule_chembl_id'),
+    (re.compile(r'((chembl\:(?P<term>chembl[0-9]+))|(chembl[0-9]+))', re.I), 'chembl.molecule_chembl_id'),
     (re.compile(r'chebi\:[0-9]+', re.I), ['chebi.id', 'chebi.secondary_chebi_id']),
-    (re.compile(r'[A-Z0-9]{10}'), 'unii.unii'),
-    (re.compile(r'db[0-9]+', re.I), ['unichem.drugbank', 'chebi.xrefs.drugbank', 'drugcentral.xrefs.drugbank_id', 'pharmgkb.xrefs.drugbank']),
-    (re.compile(r'pa[0-9]+', re.I), 'pharmgkb.id'),
-    (re.compile(r'((cid\:(?P<term>[0-9]+))|([0-9]+))', re.I), ['pubchem.cid', 'fda_orphan_drug.pubchem_sid'])
+    (re.compile(r'((unii\:(?P<term>[A-Z0-9]{10}))|([A-Z0-9]{10}))', re.I), 'unii.unii'),
+    (re.compile(r'((drugbank\:(?P<term>db[0-9]+))|(db[0-9]+))', re.I), ['unichem.drugbank', 'chebi.xrefs.drugbank', 'drugcentral.xrefs.drugbank_id', 'pharmgkb.xrefs.drugbank']),
+    (re.compile(r'((pharmgkb.drug\:(?P<term>pa[0-9]+))|(pa[0-9]+))', re.I), 'pharmgkb.id'),
+    (re.compile(r'((((pubchem.compound\:)|(cid\:))(?P<term>[0-9]+))|([0-9]+))', re.I), ['pubchem.cid']),
+    (re.compile(r'((((sid\:)|(pubchem.substance\:))(?P<term>[0-9]+))|([0-9]+))', re.I), ['fda_orphan_drug.pubchem_sid'])
 ]
 
 STATUS_CHECK = {
