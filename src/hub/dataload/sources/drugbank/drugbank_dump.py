e871bbafea3105038e00fdbc9c6661766c890bb4
Ricardo Avila <ravila@protonmail.com>
2022-08-14 23:30:49 -0400

    Rewrite drugbank datasource for new vocaulary data
    Re-write of drugbank parser, dumper, and uploader to use the new open
    source drugbank vocabulary data.
    
    The old drugbank code was moved to the folder 'fulldata_archived'.

--- src/hub/dataload/sources/drugbank/drugbank_dump.py
+++ src/hub/dataload/sources/drugbank/drugbank_dump.py
@@ -1,43 +1,41 @@
 import os
-import os.path
-import sys
-import time
+
 import bs4
-
-import biothings, config
-biothings.config_for_app(config)
-
-from config import DATA_ARCHIVE_ROOT
 from biothings.hub.dataload.dumper import HTTPDumper
+from biothings.utils.common import unzipall
+from config import DATA_ARCHIVE_ROOT
 
 
 class DrugBankDumper(HTTPDumper):
-    """
-    DrugBank requires to sign-in before downloading a file. This dumper
-    will just monitor new versions and report when a new one is available
-    """
 
     SRC_NAME = "drugbank"
     SRC_ROOT_FOLDER = os.path.join(DATA_ARCHIVE_ROOT, SRC_NAME)
-    AUTO_UPLOAD = False # it's still manual, so upload won't have the 
 
     SCHEDULE = "0 12 * * *"
-    VERSIONS_URL = "https://www.drugbank.ca/releases"
+    VERSIONS_URL = "https://go.drugbank.com/releases"
 
-    def create_todump_list(self,force=False,**kwargs):
+    def get_latest_release(self):
         res = self.client.get(self.VERSIONS_URL)
-        html = bs4.BeautifulSoup(res.text,"lxml")
-        table = html.findAll(attrs={"class":"table-bordered"})
-        assert len(table) == 1, "Expecting one table element, got %s" % len(table)
+        res.raise_for_status()
+        html = bs4.BeautifulSoup(res.text, "lxml")
+        table = html.findAll(attrs={"class": "table-bordered"})
+        assert len(table) == 1, f"Expecting one element, got {len(table)}"
         table = table.pop()
-        # the very first element in the table contains the latest version
+        # The very first element in the table contains the latest version
         version = table.find("tbody").find("tr").find("td").text
-        if force or not self.src_doc or (self.src_doc and self.src_doc.get("download",{}).get("release") < version):
-            self.release = version # new_data_folder can be generated
-            self.logger.info("DrugBank, new release '%s' available, please download it from " % version + \
-                    "https://www.drugbank.ca/releases and put the file in folder '%s'. " % self.new_data_folder + \
-                    "Once downloaded, run upload('drugbank') from the hub command line",
-                    extra={"notify":True})
-            local = os.path.join(self.new_data_folder,"releases")
-            self.to_dump.append({"remote":self.VERSIONS_URL, "local":local})
+        return version
 
+    def create_todump_list(self, force=False):
+        self.release = self.get_latest_release()
+        if force or not self.src_doc or (self.src_doc and self.src_doc.get(
+                "download", {}).get("release") < self.release):
+            remote_url = self.VERSIONS_URL + \
+                "/{}/downloads/all-drugbank-vocabulary".format(
+                    self.release.replace(".", "-")
+                )
+            local = os.path.join(self.new_data_folder,
+                                 "all_drugbank_vocabulary.zip")
+            self.to_dump.append({"remote": remote_url, "local": local})
+
+    def post_dump(self, *args, **kwargs):
+        unzipall(self.new_data_folder)
--- src/hub/dataload/sources/drugbank/drugbank_upload.py
+++ src/hub/dataload/sources/drugbank/drugbank_upload.py
@@ -1,25 +1,23 @@
 """
 DrugBank Uploader
 """
-# pylint: disable=E0401, E0611
 import os
-import glob
+
+# pylint: disable=E0401, E0611
+
 import pymongo
 import biothings.hub.dataload.storage as storage
-from biothings.utils.common import unzipall
-from biothings.utils.exclude_ids import ExcludeFieldsById
 from hub.dataload.uploader import BaseDrugUploader
 from hub.datatransform.keylookup import MyChemKeyLookup
+
 from .drugbank_mapping import drugbank_mapping
 from .drugbank_parser import load_data
-from .exclusion_ids import exclusion_ids
 
 
 SRC_META = {
-    "url" : "http://www.drugbank.ca",
-    "license_url" : "https://www.drugbank.ca/releases/latest",
-    "license_url_short" : "http://bit.ly/2PSfZTD",
-    "license" : "CC BY-NC 4.0",
+    "url": "http://go.drugbank.com",
+    "license_url": "https://go.drugbank.com/releases/latest#open-data",
+    "license": "CC0",
 }
 
 
@@ -29,48 +27,34 @@ class DrugBankUploader(BaseDrugUploader):
     """
 
     name = "drugbank"
-    storage_class = storage.IgnoreDuplicatedStorage
-    __metadata__ = {"src_meta" : SRC_META}
-    # See the comment on the ExcludeFieldsById for use of this class.
-    exclude_fields = ExcludeFieldsById(exclusion_ids, [
-        "drugbank.drug_interactions",
-        "drugbank.products",
-        "drugbank.mixtures"
-    ])
-    keylookup = MyChemKeyLookup(
-        [("inchikey", "drugbank.inchi_key"),
+    # Merge documents with the same _id
+    storage_class = storage.RootKeyMergerStorage
+    __metadata__ = {"src_meta": SRC_META}
+
+    keylookup = MyChemKeyLookup([
+         ("inchikey", "drugbank.inchikey"),
          ("drugbank", "drugbank.id"),
-         # the following keys could possible be used to lookup 'inchikey' or 'unii'
-         ("chebi", "drugbank.xrefs.chebi"),
-         ("chembl", "drugbank.xrefs.chembl"),
-         ("pubchem", "drugbank.xrefs.pubchem.cid"),
-         ("inchi", "drugbank.inchi"),
-         ("drugname", "drugbank.name"), # can be used to lookup unii, disabled for now
-        ],
-        copy_from_doc=True)
+         ("unii", "drugbank.unii"),
+         ("drugname", "drugbank.name")
+    ], copy_from_doc=True)
 
     def load_data(self, data_folder):
         """load_data from data source"""
-        xmlfiles = glob.glob(os.path.join(data_folder, "*.xml"))
-        if not xmlfiles:
-            self.logger.info("Unzipping drugbank archive")
-            unzipall(data_folder)
-            self.logger.info("Load data from '%s'" % data_folder)
-            xmlfiles = glob.glob(os.path.join(data_folder, "*.xml"))
-        assert len(xmlfiles) == 1, "Expecting one xml file, got %s" % repr(xmlfiles)
-        input_file = xmlfiles.pop()
-        assert os.path.exists(input_file), "Can't find input file '%s'" % input_file
-        return self.exclude_fields(self.keylookup(load_data, debug=True))(input_file)
+        csvfile = os.path.join(data_folder, "drugbank vocabulary.csv")
+        assert os.path.exists(csvfile), f"Can't find input file: {csvfile}"
+        return self.keylookup(load_data, debug=True)(csvfile)
 
     def post_update_data(self, *args, **kwargs):
         # pylint: disable=W0613
         """create indexes following upload"""
-        for idxname in ["drugbank.id", "drugbank.chebi", "drugbank.xrefs.chebi", "drugbank.inchi"]:
+        for idxname in [
+            "drugbank.id",
+            "drugbank.unii"
+        ]:
             self.logger.info("Indexing '%s'" % idxname)
             # background=true or it'll lock the whole database...
-            self.collection.create_index([(idxname, pymongo.HASHED)], background=True)
-        # hashed index won"t support arrays, values are small enough to standard
-        self.collection.create_index("drugbank.products.ndc_product_code")
+            self.collection.create_index([(idxname, pymongo.HASHED)],
+                                         background=True)
 
     @classmethod
     def get_mapping(cls):
