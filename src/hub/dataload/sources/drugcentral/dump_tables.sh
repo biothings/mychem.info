1970b600df51dfc24d35b42ffac6a4d62b8c6249
Dylan Welzel <dylanwelzel@gmail.com>
2023-08-29 13:41:49 -0700

    separate drugbank, update edges

--- src/hub/dataload/sources/drugcentral/dump_tables.sh
+++ src/hub/dataload/sources/drugcentral/dump_tables.sh
@@ -17,6 +17,25 @@ $CONN_STR -d $database -c "\copy (select * from act_table_full) TO act_table_ful
 $CONN_STR -d $database -c "\copy (select * from omop_relationship) TO omop_relationship.csv DELIMITER ',' CSV HEADER;"
 $CONN_STR -d $database -c "\copy (select * from approval) TO approval.csv DELIMITER ',' CSV HEADER;"
 $CONN_STR -d $database -c "\copy (select * from atc_ddd) TO drug_dosage.csv DELIMITER ',' CSV HEADER;"
-$CONN_STR -d $database -c "\copy (select * from synonyms) TO syonyms.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -d $database -c "\copy (select * from synonyms) TO synonyms.csv DELIMITER ',' CSV HEADER;"
 $CONN_STR -d $database -c "\copy (select id, inchi, inchikey, smiles, cas_reg_no, name from structures) TO structures.smiles.csv DELIMITER ',' CSV HEADER;"
 $CONN_STR -d $database -c "\copy (select * from identifier) TO identifiers.csv DELIMITER ',' CSV HEADER;"
+
+
+-------------------------
+
+#!/bin/bash
+
+CONN_STR="psql -h unmtid-dbs.net -p 5433 -U drugman -d drugcentral"
+
+# Download data tables as csv
+$CONN_STR -c "\copy (select * from pharma_class) TO pharma_class.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select * from faers) TO faers.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select * from act_table_full) TO act_table_full.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select * from omop_relationship) TO omop_relationship.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select * from approval) TO approval.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select * from atc_ddd) TO drug_dosage.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select * from synonyms) TO synonyms.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select id, inchi, inchikey, smiles, cas_reg_no, name from structures) TO structures.smiles.csv DELIMITER ',' CSV HEADER;"
+$CONN_STR -c "\copy (select * from identifier) TO identifiers.csv DELIMITER ',' CSV HEADER;"
+echo "Finished dumping tables"
