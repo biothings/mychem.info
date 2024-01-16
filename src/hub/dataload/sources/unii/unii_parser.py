e8d012c4eebe598addf06ee6f05e13fd74f10c44
Dylan Welzel <dylanwelzel@gmail.com>
2024-01-16 09:39:22 -0800

    add ncit descriptions, handle missing inchikey with pubchem fallback and logging

--- src/hub/dataload/sources/unii/unii_parser.py
+++ src/hub/dataload/sources/unii/unii_parser.py
@@ -1,5 +1,30 @@
 import pandas as pd
 from biothings.utils.dataload import int_convert
+from biothings_client import get_client
+
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+def caching_ncit_descriptions(ncit_ids):
+    """cache ncit descriptions for all unii.ncit IDs"""
+    if not ncit_ids:
+        return {}
+
+    ncit_api = get_client(url="https://biothings.ncats.io/ncit")
+    ncit_ids = [f"NCIT:{ncit}" for ncit in ncit_ids]
+    ncit_res = ncit_api.getnodes(ncit_ids, fields="def")
+    ncit_def_d = {}
+    for hit in ncit_res:
+        if hit.get("def"):
+            ncit_def = hit["def"]
+            if ncit_def.startswith('"') and ncit_def.endswith('" []'):
+                ncit_def = ncit_def[1:-4]
+            ncit_def_d[hit["_id"].replace("NCIT:", "")] = ncit_def
+    return ncit_def_d
 
 
 def load_data(input_file):
@@ -7,7 +32,7 @@ def load_data(input_file):
         unii = pd.read_csv(input_file, sep='\t', low_memory=False, dtype=str)
     except UnicodeDecodeError:
         unii = pd.read_csv(input_file, sep='\t', low_memory=False, dtype=str, encoding='windows-1252')
-    
+
     unii.rename(columns={'MF': 'molecular_formula',
                          'PT': 'preferred_term',
                          'RN': 'registry_number'}, inplace=True)
@@ -16,9 +41,14 @@ def load_data(input_file):
     # half of them don't have inchikeys
     # set the primary key to inchikey and fill in missing ones with unii
     unii['_id'] = unii.inchikey
+    unii['_id'].fillna(unii.pubchem, inplace=True)
     unii['_id'].fillna(unii.unii, inplace=True)
 
-    dupes = set(unii._id) - set(unii._id.drop_duplicates(False))
+    # Extract all unique ncit IDs
+    ncit_ids = unii["ncit"].dropna().unique().tolist()
+    ncit_descriptions = caching_ncit_descriptions(ncit_ids)
+
+    dupes = set(unii._id) - set(unii._id.drop_duplicates(keep=False))
     records = [{k: v for k, v in record.items() if pd.notnull(v)} for record in unii.to_dict("records") if record['_id'] not in dupes]
     records = [{'_id': record['_id'], 'unii': record} for record in records]
     # take care of a couple cases with identical inchikeys
@@ -27,6 +57,10 @@ def load_data(input_file):
         dr = [{k: v for k, v in record.items() if pd.notnull(v)} for record in dr]
         records.append({'_id': dupe, 'unii': dr})
     for record in records:
+        if 'ncit' in record['unii']:
+            ncit_id = record['unii']['ncit']
+            if ncit_id in ncit_descriptions:
+                record['unii']['ncit_description'] = ncit_descriptions[ncit_id]
         if isinstance(record['unii'], dict):
             del record['unii']['_id']
         else:
@@ -37,6 +71,7 @@ def load_data(input_file):
         record = int_convert(record, include_keys=['unii.pubchem'])
 
         yield record
+
 
 
 if __name__ == "__main__":
