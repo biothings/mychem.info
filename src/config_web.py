e85e6b56edcbed488d3a31250c36d2ffc17afd68
ctrl-schaff <59380780+ctrl-schaff@users.noreply.github.com>
2024-04-19 09:30:59 -0700

    Add CURIE ID query support (#173)
    * Optional CURIE prefixes in annotation regex
    
    * Add the biolink model prefixes from pending.api
    
    * Add default if no field is found
    
    * Add the keep_prefix expression [WIP]
    
    * Flush out the regex pattern tests
    
    * Add regex pattern searching to configuration
    
    * Add the CURIE ID tests
    
    * Remove the old tests from local
    
    * Remove unused imports
    
    ---------
    
    Co-authored-by: Ricardo Avila <ravila@protonmail.com>
    Co-authored-by: Johnathan Schaff <jschaff@scripps.edu>

--- src/config_web.py
+++ src/config_web.py
@@ -3,6 +3,7 @@
     https://mychem.info/
     Chemical and Drug Annotation as a Service.
 """
+
 import copy
 import re
 
@@ -15,31 +16,120 @@ ES_HOST = 'http://localhost:9200'
 ES_INDICES = {
     "chem": "mychem_current",
     "drug": "mychem_current",
-    "compound": "mychem_current"
+    "compound": "mychem_current",
 }
-ES_SCROLL_TIME = '10m'
+ES_SCROLL_TIME = "10m"
 
 # *****************************************************************************
 # Endpoint Specifics
 # *****************************************************************************
 
-ANNOTATION_ID_REGEX_LIST = [
-    (re.compile(r'chembl[0-9]+', re.I), 'chembl.molecule_chembl_id'),
-    (re.compile(r'chebi\:[0-9]+', re.I), ['chebi.id', 'chebi.secondary_chebi_id']),
-    (re.compile(r'[A-Z0-9]{10}'), 'unii.unii'),
-    (re.compile(r'db[0-9]+', re.I), ['unichem.drugbank', 'chebi.xrefs.drugbank', 'drugcentral.xrefs.drugbank_id', 'pharmgkb.xrefs.drugbank']),
-    (re.compile(r'pa[0-9]+', re.I), 'pharmgkb.id'),
-    (re.compile(r'((cid\:(?P<term>[0-9]+))|([0-9]+))', re.I), ['pubchem.cid', 'fda_orphan_drug.pubchem_sid'])
+# *** NOTE ***
+# The CHEBI prefix must have a regex_term_pattern without a named <term> grouping.
+# example query: CHEBI:57966:
+# code snippet location: <biothings.api/web/query/builder.py>
+# With a named term grouping of <term>, we produce the following which will fail
+#     named_groups = match.groupdict() -> {"term": 57966}
+#     q = named_groups.get(self.gpname.term) or q -> "57966"
+# Without a named term grouping of <term> we orduce the following which will pass
+#     named_groups = match.groupdict() -> {}
+#     q = named_groups.get(self.gpname.term) or q -> "CHEBI:57966"
+
+BIOLINK_MODEL_PREFIX_BIOTHINGS_CHEM_MAPPING = {
+    "INCHIKEY": {"type": "chem"},
+    "CHEMBL.COMPOUND": {
+        "type": "chem",
+        "field": "chembl.molecule_chembl_id",
+        "regex_term_pattern": "(?P<term>chembl[0-9]+)",
+        # "converter": lambda x: x.replace("CHEMBL.COMPOUND:", "CHEMBL"),
+    },
+    "PUBCHEM.COMPOUND": {
+        "type": "chem",
+        "field": "pubchem.cid",
+        "regex_term_pattern": "(?P<term>[0-9]+)",
+    },
+    "CHEBI": {
+        "type": "chem",
+        "field": ["chebi.id", "chebi.secondary_chebi_id"],
+        "regex_term_pattern": "(?P<term>CHEBI:[0-9]+)",
+    },
+    "UNII": {
+        "type": "chem",
+        "field": "unii.unii",
+        "regex_term_pattern": "(?P<term>[A-Z0-9]{10})",
+    },
+}
+
+# CURIE ID support based on BioLink Model
+biolink_curie_regex_list = []
+for (
+    biolink_prefix,
+    mapping,
+) in BIOLINK_MODEL_PREFIX_BIOTHINGS_CHEM_MAPPING.items():
+    field_match = mapping.get("field", [])
+    term_pattern = mapping.get("regex_term_pattern", None)
+    if term_pattern is None:
+        term_pattern = "(?P<term>[^:]+)"
+
+    raw_expression = rf"({biolink_prefix}):{term_pattern}"
+    compiled_expression = re.compile(raw_expression, re.I)
+
+    pattern = (compiled_expression, field_match)
+    biolink_curie_regex_list.append(pattern)
+
+# Custom prefix handling for chem specific identifiers
+chem_prefix_handling = [
+    (
+        re.compile(r"((chembl\:(?P<term>chembl[0-9]+))|(chembl[0-9]+))", re.I),
+        "chembl.molecule_chembl_id",
+    ),
+    (re.compile(r"chebi\:[0-9]+", re.I), ["chebi.id", "chebi.secondary_chebi_id"]),
+    (re.compile(r"((unii\:(?P<term>[A-Z0-9]{10}))|([A-Z0-9]{10}))", re.I), "unii.unii"),
+    (
+        re.compile(r"((drugbank\:(?P<term>db[0-9]+))|(db[0-9]+))", re.I),
+        [
+            "unichem.drugbank",
+            "chebi.xrefs.drugbank",
+            "drugcentral.xrefs.drugbank_id",
+            "pharmgkb.xrefs.drugbank",
+        ],
+    ),
+    (
+        re.compile(r"((pharmgkb.drug\:(?P<term>pa[0-9]+))|(pa[0-9]+))", re.I),
+        "pharmgkb.id",
+    ),
+    (
+        re.compile(
+            r"((((pubchem.compound\:)|(cid\:))(?P<term>[0-9]+))|([0-9]+))", re.I
+        ),
+        ["pubchem.cid"],
+    ),
+    (
+        re.compile(
+            r"((((sid\:)|(pubchem.substance\:))(?P<term>[0-9]+))|([0-9]+))", re.I
+        ),
+        ["fda_orphan_drug.pubchem_sid"],
+    ),
 ]
 
+default_chem_regex = re.compile(r"(?P<scope>[^:]+):(?P<term>[\W\w]+)")
+default_chem_fields = ()
+default_chem_regex_pattern = (default_chem_regex, default_chem_fields)
+
+
+ANNOTATION_ID_REGEX_LIST = [
+    *biolink_curie_regex_list,
+    *chem_prefix_handling,
+    default_chem_regex_pattern,
+]
+
+
 STATUS_CHECK = {
-    'id': 'USNINKBPBVKHHZ-CYUUQNCZSA-L',  # penicillin
-    'index': 'mychem_current',
+    "id": "USNINKBPBVKHHZ-CYUUQNCZSA-L",  # penicillin
+    "index": "mychem_current",
 }
 
-_extra_kwargs = {
-    "list_filter": {"type": str, "default": None}
-}
+_extra_kwargs = {"list_filter": {"type": str, "default": None}}
 ANNOTATION_KWARGS = copy.deepcopy(ANNOTATION_KWARGS)
 ANNOTATION_KWARGS["*"].update(_extra_kwargs)
 QUERY_KWARGS = copy.deepcopy(QUERY_KWARGS)
--- src/tests/test_local.py
+++ src/tests/test_local.py
@@ -1,9 +1,14 @@
+"""
+Local tests for exercising the annotation regex pattern matching for
+different types of queries one could expect for the mychem data sources
+"""
+
 from biothings.tests.web import BiothingsWebAppTest
 
 
 class TestMyChemWebAppConfigAnnotationIdRegex(BiothingsWebAppTest):
-    TEST_DATA_DIR_NAME = 'TestAnnotationRegex'
-    
+    TEST_DATA_DIR_NAME = "TestAnnotationRegex"
+
     """
     def test_010_drugbank(self):
         q = 'db03107'
@@ -14,46 +19,46 @@ class TestMyChemWebAppConfigAnnotationIdRegex(BiothingsWebAppTest):
     """
 
     def test_011_chembl(self):
-        q = 'chembl297569'
+        q = "chembl297569"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result(q, res, 'chembl.molecule_chembl_id', True)
+        assert self.value_in_result(q, res, "chembl.molecule_chembl_id", True)
 
     def test_012_chebi(self):
-        q = 'chebi:57966'
+        q = "chebi:57966"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result(q, res, 'chebi.id', True)
+        assert self.value_in_result(q, res, "chebi.id", True)
 
     def test_013_chebi_secondary(self):
-        q = 'chebi:22821'
+        q = "chebi:22821"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result(q, res, 'chebi.secondary_chebi_id', True)
+        assert self.value_in_result(q, res, "chebi.secondary_chebi_id", True)
 
     def test_014_unii(self):
-        q = '11P2JDE17B'
+        q = "11P2JDE17B"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result(q, res, 'unii.unii', False)
+        assert self.value_in_result(q, res, "unii.unii", False)
 
     def test_015_pubchem(self):
-        q = 'cid:120933777'
+        q = "cid:120933777"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result("120933777", res, 'pubchem.cid')
+        assert self.value_in_result("120933777", res, "pubchem.cid")
 
     def test_016_pubchem_noprefix(self):
-        q = '120933777'
+        q = "120933777"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result(q, res, 'pubchem.cid')
+        assert self.value_in_result(q, res, "pubchem.cid")
 
     """
     def test_020_drugbank_ci(self):
@@ -65,33 +70,33 @@ class TestMyChemWebAppConfigAnnotationIdRegex(BiothingsWebAppTest):
     """
 
     def test_021_chembl_ci(self):
-        q = 'CHEMBL297569'
+        q = "CHEMBL297569"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result(q, res, 'chembl.molecule_chembl_id', True)
+        assert self.value_in_result(q, res, "chembl.molecule_chembl_id", True)
 
     def test_022_chebi_ci(self):
-        q = 'ChEBI:57966'
+        q = "ChEBI:57966"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result(q, res, 'chebi.id', True)
+        assert self.value_in_result(q, res, "chebi.id", True)
 
     def test_023_cid_ci(self):
-        q = 'Cid:120933777'
+        q = "Cid:120933777"
         res = self.request("chem", method="POST", data={"ids": q})
         res = res.json()
         assert len(res) == 1
-        assert self.value_in_result("120933777", res, 'pubchem.cid')
+        assert self.value_in_result("120933777", res, "pubchem.cid")
 
 
 class TestMyChemWebAppConfigAnnotationRegexMockData(BiothingsWebAppTest):
-    TEST_DATA_DIR_NAME = 'TestAnnotationRegexMock'
+    TEST_DATA_DIR_NAME = "TestAnnotationRegexMock"
 
     def test_001_drugbank_db_in_default_scope(self):
         # by default it looks at _id, but the query below
         # matches the UNII regex, so nothing should be returned
-        q = 'DEFAULTSCO'
+        q = "DEFAULTSCO"
         # FIXME: check response status code
         res = self.request(f"chem/{q}", method="GET", expect=404)
