22bb8d57a6a64c5ee8811f4b3459e5382dfc41c4
Dylan Welzel <dylanwelzel@gmail.com>
2024-11-11 15:21:05 -0700

    add no_of_interaction_targets to gtopdb mapping

--- src/plugins/gtopdb/mapping.py
+++ src/plugins/gtopdb/mapping.py
@@ -14,6 +14,7 @@ def get_customized_mapping(cls):
                 "smiles": {"type": "keyword"},
                 "inchikey": {"type": "keyword"},
                 "inchi": {"type": "keyword"},
+                "no_of_interaction_targets": {"type": "integer"},
                 "interaction_targets": {
                     "properties": {
                         "target_id": {
